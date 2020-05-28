986fa757d0a7702eac1f23a78b4bfdd129478634
Kyle Wagner <kyle.p.wagner@gmail.com>
2020-05-27 21:14:49 -0500

    Use input with hand-range-piece component

--- src/app/hand-range-toolset/hand-range-grid-piece/hand-range-grid-piece.component.ts
+++ src/app/hand-range-toolset/hand-range-grid-piece/hand-range-grid-piece.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HandRangePiece } from 'src/app/hand-range-piece';
+
 
 @Component({
   selector: 'ht-hand-range-grid-piece',
@@ -6,11 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hand-range-grid-piece.component.css']
 })
 export class HandRangeGridPieceComponent implements OnInit {
-  
+  @Input() handRangePiece: HandRangePiece;
   
   constructor() { }
 
   ngOnInit() {
   }
+  
+  displayRank(n: number): string {
+    const displayRanks = {
+      2: '2',
+      3: '3',
+      4: '4',
+      5: '5',
+      6: '6',
+      7: '7',
+      8: '8',
+      9: '9',
+      10: 'T',
+      11: 'J',
+      12: 'Q',
+      13: 'K',
+      14: 'A'
+    }
 
+    return displayRanks[n];
+  }
 }
